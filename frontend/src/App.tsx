bb67e110fe9fd6a6fea69c3488953e73f20f70ab
Madalyn Baehre <mmbaehre@gmail.com>
2025-06-27 11:30:03 -0500

    F - initialize token client in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CalendarMonth from '@mui/icons-material/CalendarMonth';
 import './App.css';
 
+const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
+
 function App() {
+  const clientId = process.env.REACT_APP_CLIENT_ID as string;
+
+  useEffect(() => {
+    (window as any).google.accounts.oauth2.initTokenClient({
+      client_id: clientId,
+      scope: SCOPES,
+    });
+  }, [clientId]);
+
   return (
     <div className="App">
       <header className="App-header">
