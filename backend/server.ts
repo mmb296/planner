203809f6d073102742ba5185e3d253cdb0b6fee3
Madalyn Baehre <mmbaehre@gmail.com>
2025-10-28 17:20:02 -0500

    R - use PORT var instead of hardcode

--- backend/server.ts
+++ backend/server.ts
@@ -41,7 +41,7 @@ app.get('/', (req, res) => {
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  'http://localhost:5000/auth/google/callback'
+  `http://localhost:${PORT}/auth/google/callback`
 );
 
 const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
