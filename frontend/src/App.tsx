e968466b669390145bf778a242d366a574e042d0
Madalyn Baehre <mmbaehre@gmail.com>
2025-12-08 16:30:52 -0600

    R - import sort

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css';
 
 import Calendar from './components/calendar/Calendar';
-import TaskList from './components/tasks/TaskList';
 import CountdownBanner from './components/CountdownBanner';
+import TaskList from './components/tasks/TaskList';
 
 function App() {
   return (
