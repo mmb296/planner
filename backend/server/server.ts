7c126aadf1f28405d077e29a2a9142927fa0fce5
Madalyn Baehre <mmbaehre@gmail.com>
2026-04-27 16:18:46 -0500

    B - fix potentially inconsistent port number

--- backend/server/server.ts
+++ backend/server/server.ts
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 try {
   await initDatabase();
-  await initializeApp();
+  await initializeApp(PORT);
   applyErrorMiddleware(app);
   app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
